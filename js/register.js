ff5f39b29982a8b22a2215713be227266f491037
Shivam Singh <124266670+shivam-i@users.noreply.github.com>
2023-03-02 12:28:01 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
         document.getElementById("dob").addEventListener("change", function() {
           var input = this.value;
           var dateEntered = new Date(input);
-          console.log(input); //e.g. 2015-11-13
-          console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+          console.log(input);
+          console.log(dateEntered);
       });
 
         var formData = {
@@ -34,9 +34,3 @@ $(document).ready(function(){
   
       });
  });
-
-// $(function () {
-
-    
-
-//   });
